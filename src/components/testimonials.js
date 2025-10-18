91853f434d07d8c1576794dd94570c42f5adb1f0
Luis Flor <luisflorchacon7@gmail.com>
2025-10-18 18:31:22 +0200

    References limitadas a la unica que tengo

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -9,18 +9,6 @@ var testimonialsData = [
     description:'"He consistently carried out his work with punctuality, dedication, professionalism, and a strong sense of responsibility. His cheerful and helpful attitude made him a valued member of the team. Moreover, he demonstrated a proactive approach to improving his skills and taking on additional responsibilities within the company. Upon his departure, he left a significant gap among his colleagues"',
     designation:'Founder & CEO of Cidesal S.L.'
   },
-  {
-    id: 2,
-    name: 'Jasmine Perry',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, magni fugiat eveniet minus perspiciatis! Nostrum laborum maxime consequuntur repellat nam magni, quae incidunt distinctio enim itaque eligendi laboriosam, quod, ad!',
-    designation: 'Accountant'
-  },
-  {
-    id: 3,
-    name: 'Rocky Johnson',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium facilis optio porro omnis alias eaque corporis error est ut, reprehenderit quae asperiores illum quo voluptates debitis non. Repellat fugit, asperiores?',
-    designation: 'CEO'
-  }
 ]
 
 function AppTestimonials() {
