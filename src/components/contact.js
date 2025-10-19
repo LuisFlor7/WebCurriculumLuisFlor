8170e55ca370a9686778e9c8933d0c868a45f76d
Luis Flor <luisflorchacon7@gmail.com>
2025-10-19 17:46:11 +0200

    Añadido el emoji de microscopio para el ensalce a google scholar

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,6 @@
 import Container from 'react-bootstrap/Container';
 import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'; 
+import { faEnvelope, faMicroscope } from '@fortawesome/free-solid-svg-icons'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function AppContact() {
@@ -18,6 +18,7 @@ function AppContact() {
           <li><a href="https://www.linkedin.com/in/luis-flor-chacon/"><FontAwesomeIcon icon={faLinkedinIn} /></a></li>
           <li><a href="mailto:luisflorchacon7@gmail.com"><FontAwesomeIcon icon={faEnvelope} /></a></li>
           <li><a href="https://github.com/LuisFlor7"><FontAwesomeIcon icon={faGithub} /></a></li>
+          <li><a href="https://scholar.google.es/citations?user=qbD7KTQAAAAJ&hl=es&oi=ao"><FontAwesomeIcon icon={faMicroscope} /></a></li>
         </ul>
       </div>
       </Container>
