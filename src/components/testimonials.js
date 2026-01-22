a1f032f1d720d69b8fb3f3735c81fc963a66b37b
silvia chacon dorantes <luisflorchacon7@gmail.com>
2026-01-22 12:28:37 +0100

    Añadida referencia de Pepe Escaona

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -9,6 +9,12 @@ var testimonialsData = [
     description:'"He consistently carried out his work with punctuality, dedication, professionalism, and a strong sense of responsibility. His cheerful and helpful attitude made him a valued member of the team. Moreover, he demonstrated a proactive approach to improving his skills and taking on additional responsibilities within the company. Upon his departure, he left a significant gap among his colleagues"',
     designation:'Founder & CEO of Cidesal S.L.'
   },
+  {
+    id: 2,
+    name:'José Mariano Escalona',
+    description:'"I hereby state that Mr. Luis Flor Chacón has very satisfactorily fulfilled the objectives and tasks assigned since his incorporation in 2023 into INAGEA as a predoctoral researcher up to the present date. During this period, he has acquired solid expertise in techniques and methodologies applied to plant physiology and has actively participated in research projects, demonstrating a strong ability to work effectively as part of a team. Furthermore, he has been actively involved in knowledge transfer activities, where he has shown excellent communication skills."',
+    designation:'President at INAGEA-UIB, University professor'
+  }
 ]
 
 function AppTestimonials() {
